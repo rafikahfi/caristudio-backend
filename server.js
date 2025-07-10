2b1807996f10c2aff3336ddfad79a598f73cff54
Rafi Kahfi Yugi <rafikahfi01@gmail.com>
2025-07-10 12:19:06 +0700

    Menambahkan module.exports di server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const path = require("path");
 
 const studioRoutes = require("./routes/studioRoutes");
 const adminRoutes = require("./routes/admin");
+const { Server } = require("http");
 
 const app = express();
 
@@ -60,3 +61,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server jalan di http://localhost:${PORT}`);
 });
+
+module.exports = app;
